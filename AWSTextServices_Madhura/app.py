7db05438a823eac633cbab202c13eaa317501971
madhura-u <68042077+madhura-u@users.noreply.github.com>
2021-11-10 10:57:52 +0530

    Add files via upload

---
+++ AWSTextServices_Madhura/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template,url_for
+from flask_cors import cross_origin
+import boto3
+
+app = Flask(__name__)
+
+@app.route("/")
+@cross_origin()
+def home():
+    return render_template("index.html")
+
+@app.route("/sound", methods = ["POST"])
+@cross_origin()
+def sound():
+    if request.method == "POST":
+        text = request.form['texttospeech']    
+        polly = boto3.client(service_name='polly',region_name='us-east-1')  
+        response = polly.synthesize_speech(OutputFormat='mp3', VoiceId='Joanna',Text=text)
+        file = open('static/audio/speech.mp3', 'wb')
+        file.write(response['AudioStream'].read())
+        file.close()
+    return render_template("index.html",conversion="Converted to speech")
+
+@app.route("/translation", methods = ["POST"])
+@cross_origin()
+def translation():
+    if request.method == "POST":
+        text = request.form['texttospeech']    
+        translate = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
+        result = translate.translate_text(Text=text, SourceLanguageCode="en", TargetLanguageCode="kn")
+    return render_template("index.html", trans=result.get('TranslatedText'))
+  
+if __name__ == "__main__":
+    app.run(debug=True)
